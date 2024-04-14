25decf93f5c455813796bbad8f05f44255d976b4
KohnoA <san.kohno@yandex.ru>
2024-04-14 15:35:33 +0300

    feat: update apollo server config

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { resolvers } from './resolvers.js';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  playground: true,
+  introspection: true,
 });
 
 const { url } = await startStandaloneServer(server, {
